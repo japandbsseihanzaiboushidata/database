d98e9498e35c039018f4ebeea0879ba704fcfb6f
japandbsseihanzaiboushidata <japandbsseihanzaiboushidata@gmail.com>
2024-05-08 20:05:23 +0900

    Update st.py

--- st.py
+++ st.py
@@ -7,6 +7,10 @@ import datetime
 import streamlit as st
 import pandas as pd
 import spacy
+subprocess.run([f"{sys.executable}", "-m","spacy","download","ja_core_news_md"])
+
+   
+
 
 st.title("Japan_DBS_Seihanzai_Boushi_Dataset")
 # CSVファイルを読み込む
